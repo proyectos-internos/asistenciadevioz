1d5b2d824442a03a36a5c6796ba5039c0520b3ea
Darwin <83663953+Gadalos@users.noreply.github.com>
2024-01-24 11:32:27 -0500

    Fix bug

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('localhost', 'root', '', 'nomina');
+    $conn = new mysqli('localhost', 'root', '', 'nomina2');
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
